6b80a57e08c88be92338782d8c7b8cb2762c86fc
brendon-joao-dev <brendonjoaodev@gmail.com>
2026-09-15 19:08:48 -0300

    Reorganização das aulas

---
+++ curso_youtube/lib/aula02_stateless_widget/main.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(AppWidget(title: "Eu fui escrito numa variável"));
+}
+
+class AppWidget extends StatelessWidget {
+  // Declaração de uma propriedade
+  final String title;
+
+  // Criação do construtor da classe
+  const AppWidget({super.key, required this.title});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.white70,
+      child: Center(
+        child: Text(
+          title,
+          textDirection: TextDirection.ltr,
+          style: TextStyle(color: Colors.black, fontSize: 50.0),
+        ),
+      ),
+    );
+  }
+}
--- stateless_statefull/android/app/src/main/kotlin/com/example/stateless_statefull/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.stateless_statefull
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
